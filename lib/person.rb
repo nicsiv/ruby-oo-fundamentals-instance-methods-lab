c9bf54b6b3a0c5d5605fc44622b3fc0e906ded45
nicsiv <nicoleusivek@gmail.com>
2020-09-15 11:48:28 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+    def talk
+      puts "Hello World!"
+      
+      def walk
+        puts "The Person is walking"
+       end
+    end
+  end
+   
+
+  nicole = Person.new
+  nicole.talk
+
